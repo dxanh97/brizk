d8ca9e7ed05f92b9f6e15e30b27c5d78d9bef873
Xanh <dxanh97@gmail.com>
2023-03-20 05:30:19 +0700

    ✨ add routing

---
+++ app/index.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable global-require */
+import React, { useState } from "react";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import styled, { ThemeProvider } from "styled-components";
+
+import AddTransactionBottomSheet from "../components/AddTransactionBottomSheet";
+import AppBar from "../components/AppBar";
+
+import theme from "../utils/theme";
+
+const AppWrapper = styled(View)<{ insetTop: number }>`
+  background-color: ${(props) => props.theme.background};
+  flex: 1;
+  padding-top: ${(props) => props.insetTop}px;
+`;
+const StyledGestureHandlerRootView = styled(GestureHandlerRootView)`
+  flex: 1;
+`;
+
+const AppContent: React.FC = () => {
+  const [fontLoaded] = useFonts({
+    "DM-Mono": require("../assets/fonts/DM-Mono.ttf"),
+    "DM-Sans": require("../assets/fonts/DM-Sans.ttf"),
+    "PT-Mono": require("../assets/fonts/PT-Mono.ttf"),
+  });
+  const { top: insetTop } = useSafeAreaInsets();
+  const [openAddTransaction, setOpenAddTransaction] = useState(true);
+
+  if (!fontLoaded) return null;
+  return (
+    <AppWrapper insetTop={insetTop}>
+      <StatusBar style="inverted" />
+      <AppBar />
+      <StyledGestureHandlerRootView>
+        <AddTransactionBottomSheet
+          open={openAddTransaction}
+          onClose={() => setOpenAddTransaction(false)}
+        />
+      </StyledGestureHandlerRootView>
+    </AppWrapper>
+  );
+};
+
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <SafeAreaProvider>
+      <AppContent />
+    </SafeAreaProvider>
+  </ThemeProvider>
+);
+
+export default App;
