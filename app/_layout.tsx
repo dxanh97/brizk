a8560080fb5f9fbc0e8673830053e22cbc03f3e0
Xanh <dxanh97@gmail.com>
2023-04-09 00:04:57 +0700

    ♻️ implement Layout

---
+++ app/_layout.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable global-require */
+import React from "react";
+import { View } from "react-native";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import styled, { ThemeProvider } from "styled-components";
+
+import store from "../store";
+import defaultTheme from "../utils/theme";
+
+import AppBar from "../components/AppBar";
+
+const AppWrapper = styled(View)<{ insetTop: number }>`
+  flex: 1;
+  background-color: ${({ theme }) => theme.neutral.get(1)};
+  padding: ${(props) => props.insetTop}px 16px 0;
+`;
+const StyledGestureHandlerRootView = styled(GestureHandlerRootView)`
+  flex: 1;
+`;
+
+const Layout: React.FC = () => {
+  const [fontLoaded] = useFonts({
+    "DM Mono": require("../assets/fonts/DM-Mono.ttf"),
+    "DM Sans": require("../assets/fonts/DM-Sans.ttf"),
+    "PT Mono": require("../assets/fonts/PT-Mono.ttf"),
+  });
+  const { top: insetTop } = useSafeAreaInsets();
+
+  if (!fontLoaded) return null;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={defaultTheme}>
+        <SafeAreaProvider>
+          <AppWrapper insetTop={insetTop}>
+            <StatusBar style="inverted" />
+            <AppBar />
+            <StyledGestureHandlerRootView>
+              <Slot />
+            </StyledGestureHandlerRootView>
+          </AppWrapper>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default Layout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,65 +1,18 @@
 /* eslint-disable global-require */
-import React, { useState } from "react";
-import { Provider } from "react-redux";
+import React from "react";
 import { View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { useFonts } from "expo-font";
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 
-import store from "../store";
-import _theme from "../utils/theme";
-
-import AddTransactionBottomSheet from "../components/AddTransactionBottomSheet";
-import AppBar from "../components/AppBar";
 import Summary from "../components/Summary";
 
-const AppWrapper = styled(View)<{ insetTop: number }>`
-  background-color: ${({ theme }) => theme.neutral.get(1)};
-  flex: 1;
-  padding: ${(props) => props.insetTop}px 16px 0;
-`;
-const StyledGestureHandlerRootView = styled(GestureHandlerRootView)`
+const Wrapper = styled(View)`
   flex: 1;
 `;
-
-const AppContent: React.FC = () => {
-  const [fontLoaded] = useFonts({
-    "DM Mono": require("../assets/fonts/DM-Mono.ttf"),
-    "DM Sans": require("../assets/fonts/DM-Sans.ttf"),
-    "PT Mono": require("../assets/fonts/PT-Mono.ttf"),
-  });
-  const { top: insetTop } = useSafeAreaInsets();
-  const [openAddTransaction, setOpenAddTransaction] = useState(false);
-
-  if (!fontLoaded) return null;
-  return (
-    <AppWrapper insetTop={insetTop}>
-      <StatusBar style="inverted" />
-      <AppBar />
-      <StyledGestureHandlerRootView>
-        <Summary />
-        <AddTransactionBottomSheet
-          open={openAddTransaction}
-          onClose={() => setOpenAddTransaction(false)}
-        />
-      </StyledGestureHandlerRootView>
-    </AppWrapper>
-  );
-};
 
 const App: React.FC = () => (
-  <Provider store={store}>
-    <ThemeProvider theme={_theme}>
-      <SafeAreaProvider>
-        <AppContent />
-      </SafeAreaProvider>
-    </ThemeProvider>
-  </Provider>
+  <Wrapper>
+    <Summary />
+  </Wrapper>
 );
 
 export default App;
